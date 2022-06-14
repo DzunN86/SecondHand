5ed150fdf8ec5efa0ff130ad8cb590cef77e6929
Juniza Permata Nugraheni <juniza.permata@students.utdi.ac.id>
2022-06-14 22:11:45 +0700

    update App js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import { View, Text } from 'react-native';
+import React, { useRef } from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react'
+import { Persistore, Store } from './store';
+import { NavigationContainer } from '@react-navigation/native'
+import Router from './navigation';
 
+const MainApp = () => {
+  const routeNameRef = useRef();
+  const navigationRef = useRef();
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={async () => {
+        routeNameRef.current = navigationRef.current.getCurrentRoute().name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        routeNameRef.current = currentRouteName;
+      }}>
+      <Router />
+    </NavigationContainer>
+  );
+};
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <Provider store={Store}>
+      <PersistGate loading={null} persistor={Persistore}>
+        <MainApp/>
       </PersistGate>
     </Provider>
   );
