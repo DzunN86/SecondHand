06efd8193757504da7ede96be491a7c557626789
Juniza Permata Nugraheni <junizanugraheni@gmail.com>
2022-06-16 14:40:41 +0700

    ...

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,10 +1,11 @@
 import {Text, View} from 'react-native';
 import React from 'react';
+import CustomHeader from '../../components/atoms/CustomHeader';
 
-export default function Login() {
+export default function Login({navigation}) {
   return (
     <View>
-      <Text>Login</Text>
+      <CustomHeader type='BackTitle'  onPress={() => navigation.navigate('')}/>
     </View>
   );
 }
