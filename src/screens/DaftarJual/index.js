e12037efd4e0e886aab1b70d3a32d9fc2a473cf7
Juniza Permata Nugraheni <junizanugraheni@gmail.com>
2022-06-21 11:35:32 +0700

    Slicing Screen DaftarJual

--- src/screens/DaftarJual/index.js
+++ src/screens/DaftarJual/index.js
@@ -1,10 +1,26 @@
-import {Text, View} from 'react-native';
+import { Text, View, ScrollView, Image } from 'react-native';
 import React from 'react';
+import HeaderTitle from '../../components/atoms/CustomHeader/Title';
+import CardCategory from '../../components/atoms/CustomCard';
+import CardSeller from '../../components/molecules/CardSeller';
 
 export default function DaftarJual() {
   return (
     <View>
-      <Text>DaftarJual</Text>
+      <HeaderTitle title='Daftar Jual Saya' />
+      <CardSeller 
+      name='Nama penjual'
+      city='Kota'
+      title='Edit'
+      />
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={{ flexDirection: 'row' , marginTop: 10}}>
+          <CardCategory icon='box' title='Produk' />
+          <CardCategory icon='heart' title='Diminati' />
+          <CardCategory icon='dollar-sign' title='Terjual' />
+        </View>
+      </ScrollView>
+
     </View>
   );
 }
