d2011e3e5c7e2c0f126c29dca2a91847968db892
Juniza Permata Nugraheni <junizanugraheni@gmail.com>
2022-06-22 20:15:13 +0700

    Update index.js

--- src/screens/FormDetail/index.js
+++ src/screens/FormDetail/index.js
@@ -1,10 +1,24 @@
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
+import BackTitle from '../../components/atoms/CustomHeader/BackTitle';
+import Input from '../../components/atoms/CustomInput'
+import { COLORS } from '../../themes';
 
-export default function FormDetail() {
+export default function FormDetail({ navigation }) {
   return (
-    <View>
-      <Text>FormDetail</Text>
-    </View>
+    <>
+      <BackTitle title='Lengkapi Detail Produk' onPress={() => navigation.navigate('Daftar Jual')} />
+      <View style={{ marginVertical: 20, marginHorizontal: 25 }}>
+        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Nama Produk</Text>
+        <Input value='Nama Produk' />
+        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Harga Produk</Text>
+        <Input value='Rp 0,00' />
+        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Kategori</Text>
+        <Input value='Pilih Kategori' />
+        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Harga Produk</Text>
+        <Input value='Pilih Kategori' style={{ height: 50 }} />
+        <Text style={{ fontSize: 18, fontWeight: 'bold', color: COLORS.black }}>Foto Produk</Text>
+      </View>
+    </>
   );
 }
