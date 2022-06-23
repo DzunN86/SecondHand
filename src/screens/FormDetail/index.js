604a36114cd5e3f196c465e213e32bbab33af78c
Juniza Permata Nugraheni <junizanugraheni@gmail.com>
2022-06-23 10:36:15 +0700

    update form detail screen

--- src/screens/FormDetail/index.js
+++ src/screens/FormDetail/index.js
@@ -1,23 +1,40 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import BackTitle from '../../components/atoms/CustomHeader/BackTitle';
-import Input from '../../components/atoms/CustomInput'
-import { COLORS } from '../../themes';
+import { CustomInput, CustomButton } from '../../components/atoms/';
+import Icon from 'react-native-vector-icons/Feather';
+import styles from './styles';
 
+const IconSize = 35
+const TabAdd = ({ icon, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} >
+      <View style={styles.container}>
+        <Icon name={icon} size={IconSize} style={styles.icon} />
+      </View>
+    </TouchableOpacity>
+  )
+}
 export default function FormDetail({ navigation }) {
   return (
     <>
       <BackTitle title='Lengkapi Detail Produk' onPress={() => navigation.navigate('Daftar Jual')} />
-      <View style={{ marginVertical: 20, marginHorizontal: 25 }}>
-        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Nama Produk</Text>
-        <Input value='Nama Produk' />
-        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Harga Produk</Text>
-        <Input value='Rp 0,00' />
-        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Kategori</Text>
-        <Input value='Pilih Kategori' />
-        <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.black }}>Harga Produk</Text>
-        <Input value='Pilih Kategori' style={{ height: 50 }} />
-        <Text style={{ fontSize: 18, fontWeight: 'bold', color: COLORS.black }}>Foto Produk</Text>
+      <View style={{ marginVertical: 10, marginHorizontal: 25 }}>
+        <CustomInput label="Nama Produk" placeholder="Nama Produk" />
+        <CustomInput label="Harga Produk" placeholder='Rp 0,00' />
+        <CustomInput label="Kategori" placeholder='Pilih Kategori' />
+        <CustomInput
+          label="Deskripsi"
+          placeholder='Contoh: Jalan Ikan Kerapu 21'
+        />
+        <Text style={styles.titleText}>Foto Produk</Text>
+        <TabAdd
+          icon='plus'
+        />
+      </View>
+      <View style={{ marginVertical: 210, marginHorizontal: 25, flexDirection: 'column' }}>
+        <CustomButton primary title='Preview' style={styles.button1} onPress={() => navigation.navigate('')} />
+        <CustomButton primary title='Terbitkan' style={styles.button2} onPress={() => navigation.navigate('')} />
       </View>
     </>
   );
---
+++ src/screens/FormDetail/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { COLORS, SIZES, FONTS } from '../../themes';
+
+export default StyleSheet.create({
+    button1: {
+        position: "absolute",
+        bottom: 10,
+        width: SIZES.width * 0.4,
+        alignSelf: "flex-start"
+    },
+    button2: {
+        position: "absolute",
+        bottom: 10,
+        width: SIZES.width * 0.4,
+        alignSelf: "flex-end"
+    },
+    titleText: {
+        color: COLORS.black,
+        ...FONTS.tabBarLabel,
+    },
+    container: {
+        width: 96,
+        height: 96,
+        borderRadius: 10,
+        backgroundColor: COLORS.white,
+        borderColor: COLORS.lightGray2,
+        paddingLeft: 16,
+        paddingRight: 14,
+        marginTop: 10,
+        marginLeft: 1,
+        marginRight: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+    },
+    icon: {
+        color: COLORS.lightGray2,
+        alignItems: 'center'
+    }
+})
