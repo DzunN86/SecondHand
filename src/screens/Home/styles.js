c0ae36aca9ec5221ffa6e75e1988566044c5f3a3
Dzun <nurroinz@gmail.com>
2022-06-24 12:09:23 +0700

    integrasi api product

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,81 +1,85 @@
 import {StyleSheet} from 'react-native';
-import {COLORS} from '../../themes';
+import {COLORS, FONTS, SIZES} from '../../themes';
 
 export default StyleSheet.create({
   search: {
-    backgroundColor:COLORS.white,
-    paddingVertical:0,
-    paddingHorizontal:20,
-    marginHorizontal:16,
+    backgroundColor: COLORS.white,
+    paddingHorizontal: 25,
+    marginHorizontal: 16,
     borderRadius: 16,
-    marginTop:38,
+    marginTop: 38,
     height: 48,
-    flexDirection:"row",
-    alignItems:"center"
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   searchKategori: {
-    backgroundColor:"#E2D4F0",
-    paddingVertical:8,
-    paddingHorizontal:8,
+    backgroundColor: '#E2D4F0',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
     borderRadius: 12,
-    marginTop:6,
+    marginTop: 6,
+    width: 110,
     marginLeft: 16,
-    flexDirection:"row",
-    alignItems:"center"    
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  labelCategory: {
+    ...FONTS.body4,
   },
   cardProduct: {
-    backgroundColor:COLORS.white,
-    marginBottom: 30,
+    backgroundColor: COLORS.white,
+    marginTop: 15,
     borderRadius: 10,
     borderColor: '#E5E5E5',
     borderWidth: 1,
-    width: 156,
-    height: 206,
-    marginLeft: 16,
-    marginTop: 5,
-    paddingHorizontal: 8,
-    paddingVertical: 8,
+    height: SIZES.height * 0.26,
+    width: SIZES.width * 0.43,
+    padding: 8,
   },
   textCardProduct: {
-    fontSize: 14, 
-    marginTop: 8, 
-    fontWeight: '400'
+    fontSize: 14,
+    marginTop: 8,
+    fontWeight: '400',
   },
   telusuriKategori: {
-    paddingLeft: 16, 
-    fontSize: 18, 
-    marginVertical:10
+    ...FONTS.h4,
+    paddingLeft: 16,
+    fontSize: 18,
+    marginVertical: 10,
   },
   typeProduct: {
-    fontSize: 10, 
-    marginTop: 4
+    fontSize: 10,
+    marginTop: 4,
   },
   cardProductWrapper: {
-    marginVertical: 27
+    marginVertical: 27,
+    marginHorizontal: 16,
   },
   textSearch: {
-    fontSize:18, 
-    width:260
+    ...FONTS.body4,
+    color: COLORS.gray1,
   },
   card: {
     paddingVertical: 32,
     paddingHorizontal: 16,
     flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   textIklan: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    ...FONTS.h2,
+    color: COLORS.black,
   },
   diskonWrapper: {
     marginTop: 16,
   },
   textDiskon: {
-    fontSize: 14,
-    marginBottom: 4,
+    ...FONTS.body4,
   },
   persenDiskon: {
     fontSize: 24,
-    color: '#FA2C5A',
+    color: COLORS.danger,
     fontWeight: 'bold',
   },
   imageIklan: {
@@ -87,5 +91,5 @@ export default StyleSheet.create({
     borderRadius: 4,
     height: 100,
     width: '100%',
-  }
+  },
 });
