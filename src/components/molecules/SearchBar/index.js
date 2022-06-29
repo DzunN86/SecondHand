c2a55738926ec0dec601cd76b57b3707768e9863
Dzun <nurroinz@gmail.com>
2022-06-29 10:51:20 +0700

    create searchBar component

---
+++ src/components/molecules/SearchBar/index.js
@@ -0,0 +1,23 @@
+import {Text, TouchableOpacity, View} from 'react-native';
+import React, {memo} from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {COLORS} from '../../../themes';
+import PropTypes from 'prop-types';
+import styles from './styles';
+
+const SearchBar = ({onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.search}>
+        <Text style={styles.textSearch}>Cari di Second Chance</Text>
+        <Icon name="search" color={COLORS.gray} size={25} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+SearchBar.PropTypes = {
+  onPress: PropTypes.func,
+};
+
+export default memo(SearchBar);
