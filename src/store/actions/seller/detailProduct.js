eb5c08b0c9fd5ed779ce8751059403fbcb1702d0
Juniza Permata Nugraheni <junizanugraheni@gmail.com>
2022-07-08 21:45:27 +0700

    add action delete and detail product seller

---
+++ src/store/actions/seller/detailProduct.js
@@ -0,0 +1,36 @@
+import {
+  GET_DETAIL_SELLER_LOADING,
+  GET_DETAIL_SELLER_FAILED,
+  GET_DETAIL_SELLER_SUCCESS
+} from "../../types";
+import { detailProduct } from "../../../services/api/seller";
+import { showError } from "../../../plugins";
+
+export const setDetailSellerSuccess = value => ({
+  type: GET_DETAIL_SELLER_SUCCESS,
+  payload: value,
+});
+
+export const setDetailSellerLoading = loading => ({
+  type: GET_DETAIL_SELLER_LOADING,
+  payload: loading,
+});
+
+export const setDetailSellerFailed = error => ({
+  type: GET_DETAIL_SELLER_FAILED,
+  payload: error
+});
+
+export const getDetailSeller = (id) => async dispatch => {
+  dispatch(setDetailSellerLoading(true));
+  await detailProduct(id)
+    .then(res => {
+      dispatch(setDetailSellerSuccess(res.data));
+      dispatch(setDetailSellerLoading(false));
+    })
+    .catch(err => {
+      dispatch(setDetailSellerFailed(err.message));
+      dispatch(setDetailSellerLoading(false));
+      showError(err.message);
+    });
+};
