3a2cfc90a289e34798333a746d1639223081d2fe
Dzun <nurroinz@gmail.com>
2022-07-11 09:35:18 +0700

    ui splash screen

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,10 +1,45 @@
-import {Text, View} from 'react-native';
-import React from 'react';
 
-export default function Splash() {
+
+import LottieView from 'lottie-react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import { logoSplash } from '../../assets';
+import {COLORS, FONTS, SIZES} from '../../themes';
+
+export default function Splash({navigation}) {
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <LottieView
+        source={logoSplash}
+        style={styles.logoImage}
+        autoPlay
+        loop={false}
+        onAnimationFinish={() => {
+            navigation.replace('MainApp')
+        }}
+      />
+      <Text style={styles.copyright}>Made with ❤️</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  copyright: {
+    ...FONTS.body4,
+    color: COLORS.lightGray,
+    position: 'absolute',
+    bottom: 19,
+    letterSpacing: 7,
+  },
+  logoImage: {
+    height: SIZES.height * 0.5,
+    width: SIZES.width * 0.5,
+    alignSelf: 'center',
+  },
+});
