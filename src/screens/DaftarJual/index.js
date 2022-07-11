b809da1f755fe6601ae2f5bc53422164698b5b80
Dzun <nurroinz@gmail.com>
2022-07-12 01:20:45 +0700

    daftar jual product seller done

--- src/screens/DaftarJual/index.js
+++ src/screens/DaftarJual/index.js
@@ -1,75 +1,89 @@
-import { View, FlatList, ActivityIndicator } from 'react-native';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {ActivityIndicator, ScrollView, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {CardCategory, CardProduct, TabAdd} from '../../components';
 import HeaderTitle from '../../components/atoms/CustomHeader/Title';
 import CardSeller from '../../components/molecules/CardSeller';
-import { CardCategory, CardProduct } from '../../components';
-import { getProductSeller } from '../../store/actions/seller/getProduct';
-import styles from './styles';
-import { COLORS } from '../../themes';
-import { doGetProfile } from '../../store/actions';
+import {doGetProfile} from '../../store/actions';
+import {getProductSeller} from '../../store/actions/seller/getProduct';
+import {COLORS, SIZES} from '../../themes';
 
-export default function DaftarJual({ navigation }) {
+export default function DaftarJual({navigation}) {
   const dispatch = useDispatch();
-  const { productSeller, isLoading } = useSelector(state => state.productSellerReducers)
-  const { userProfile } = useSelector(state => state.getUserReducer);
-  const { userData } = useSelector(state => state.loginReducer);
+  const {productSeller, isLoading} = useSelector(
+    state => state.productSellerReducers,
+  );
+  const {userProfile} = useSelector(state => state.getUserReducer);
+  const {userData} = useSelector(state => state.loginReducer);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
-    if (userData.access_token) {
+    if (isFocused) {
       dispatch(doGetProfile());
       dispatch(getProductSeller());
-
     }
-  }, [dispatch]);
+  }, [dispatch, isFocused]);
 
   return (
-    <View>
-      <HeaderTitle title='Daftar Jual Saya' />
+    <ScrollView>
+      <HeaderTitle title="Daftar Jual Saya" />
       <CardSeller
-        name={userData.access_token ? userProfile.full_name || userData.name : 'Akun'}
-        city={userData.access_token ? userProfile.city || userData.name : 'Akun'}
-        title='Edit'
+        name={
+          userData.access_token
+            ? userProfile.full_name || userData.name
+            : 'Akun'
+        }
+        city={
+          userData.access_token ? userProfile.city || userData.name : 'Akun'
+        }
+        title="Edit"
         onPress={() => navigation.navigate('InfoAkunScreen')}
-        source={{ uri: userProfile.image_url }}
+        source={{uri: userProfile.image_url}}
       />
-      <View style={{ flexDirection: 'row', marginTop: 10, marginHorizontal: 15 }}>
-        <CardCategory icon='box' active title='Produk' />
-        <CardCategory icon='heart' title='Diminati' />
-        <CardCategory icon='dollar-sign' title='Terjual' />
+      <View style={{flexDirection: 'row', marginTop: 16, marginHorizontal: 15}}>
+        <CardCategory icon="box" active title="Produk" />
+        <CardCategory icon="heart" title="Diminati" />
+        <CardCategory icon="dollar-sign" title="Terjual" />
       </View>
-      <View>
-        {/* <TabAdd
-          type='TabProduk'
-          title='Tambah Produk'
-          icon='plus'
-          onPress={() => navigation.navigate('FormDetailScreen')}
-        /> */}
+      <View
+        style={{
+          flexWrap: 'wrap',
+          marginVertical: SIZES.base,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 16,
+        }}>
+        {productSeller.length < 5 && (
+          <TabAdd
+            type="TabProduk"
+            title="Tambah Produk"
+            icon="plus"
+            onPress={() => navigation.navigate('FormDetailScreen')}
+          />
+        )}
         {isLoading ? (
           <ActivityIndicator
-            style={{ flex: 1 }}
+            style={{flex: 1}}
             size="large"
             color={COLORS.primary}
           />
         ) : (
-          <FlatList
-            numColumns={2}
-            showsVerticalScrollIndicator={false}
-            columnWrapperStyle={styles.cardProductWrapper}
-            data={productSeller}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
+          productSeller.map(item => (
+            <View key={item.id} style={{marginBottom: SIZES.base}}>
               <CardProduct
                 name={item.name}
                 price={item.base_price}
                 category={item.Categories}
                 image={item.image_url}
-                onPress={() => navigation.navigate('DetailProductSeller', { id: item.id })}
+                onPress={() =>
+                  navigation.navigate('DetailProductSeller', {id: item.id})
+                }
               />
-            )}
-          />
+            </View>
+          ))
         )}
       </View>
-    </View>
+    </ScrollView>
   );
-};
+}
