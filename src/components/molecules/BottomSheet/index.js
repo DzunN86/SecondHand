0cb7fa061f3d8125074e28b258005cb20ede6aba
Dzun <nurroinz@gmail.com>
2022-07-11 19:25:57 +0700

    create component BottomSheet

---
+++ src/components/molecules/BottomSheet/index.js
@@ -0,0 +1,47 @@
+import React, { useMemo, useCallback } from 'react';
+import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet';
+import { COLORS } from '../../../themes';
+
+function BottomSheetComponent({ sheetRef, component }) {
+  // variables
+  const snapPoints = useMemo(() => ['1%', '1%', '50%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  // const handleClosePress = () => sheetRef.current.close();
+
+  // renders
+  const renderBackdrop = useCallback(
+    (props) => (
+      <BottomSheetBackdrop
+        {...props}
+        pressBehavior="close"
+        disappearsOnIndex={1}
+        appearsOnIndex={2}
+      />
+    ),
+    [],
+  );
+
+  return (
+    <BottomSheet
+      index={0}
+      ref={sheetRef}
+      snapPoints={snapPoints}
+      enableHandlePanningGesture
+      enableContentPanningGesture
+      enableOverDrag
+      handleIndicatorStyle={{backgroundColor: COLORS.grey8}}
+      animateOnMount
+      backdropComponent={renderBackdrop}
+      onChange={handleSheetChanges}
+    >
+      {component}
+    </BottomSheet>
+  );
+}
+
+export default BottomSheetComponent;
