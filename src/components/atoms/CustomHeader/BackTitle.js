2eb8ac640e9c01b84a230ba576d93c09a8df529c
Dzun <nurroinz@gmail.com>
2022-07-15 22:25:19 +0700

    Update BackTitle.js

--- src/components/atoms/CustomHeader/BackTitle.js
+++ src/components/atoms/CustomHeader/BackTitle.js
@@ -1,41 +1,45 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { COLORS, SIZES } from '../../../themes'
-import Icon from 'react-native-vector-icons/Feather'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {COLORS, FONTS, SIZES} from '../../../themes';
+import Icon from 'react-native-vector-icons/Feather';
 
-const IconSize = 24
-function BackTitle({ title, onPress }) {
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity>
-                <Icon name='arrow-left' size={IconSize} color={COLORS.black} onPress={onPress} />
-            </TouchableOpacity>
-            <View style={styles.content}>
-                <Text style={styles.title}>{title}</Text>
-            </View>
-        </View>
-    )
+const IconSize = 24;
+function BackTitle({title, onPress}) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Icon
+          name="arrow-left"
+          size={IconSize}
+          color={COLORS.black}
+          onPress={onPress}
+        />
+      </TouchableOpacity>
+      <View style={styles.content}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </View>
+  );
 }
 
-export default BackTitle
+export default BackTitle;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: COLORS.white,
-        paddingVertical: 20,
-        paddingLeft: SIZES.padding,
-        paddingRight: SIZES.padding2,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    content: {
-        flex: 1,
-    },
-    title: {
-        fontSize: 20,
-        color: COLORS.black,
-        marginTop: 4,
-        alignSelf: 'center'
-    }
-
-})
+  container: {
+    backgroundColor: COLORS.white,
+    paddingVertical: 20,
+    paddingLeft: SIZES.padding,
+    paddingRight: SIZES.padding2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    ...FONTS.h3,
+    color: COLORS.black,
+    marginTop: 4,
+    alignSelf: 'center',
+  },
+});
