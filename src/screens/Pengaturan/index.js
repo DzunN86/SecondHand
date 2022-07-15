59529e0691b667abf074dbd212f4ae759add7048
Galang Sumantri <galanksumantri@gmail.com>
2022-07-15 21:21:10 +0700

    formik change pw

--- src/screens/Pengaturan/index.js
+++ src/screens/Pengaturan/index.js
@@ -1,15 +1,68 @@
 import { View } from 'react-native';
 import React from 'react';
-import { CustomHeader } from '../../components';
+import { CustomHeader, CustomInput, CustomButton } from '../../components';
+import { Formik } from 'formik';
+import styles from './styles';
 
 export default function Settings({navigation}) {
   return (
     <View>
-       <CustomHeader
+      <CustomHeader
         type="BackTitle"
         title="Pengaturan Akun"
         onPress={() => navigation.goBack()}
       />
+      <View style={styles.form}>
+          <Formik
+            initialValues={{
+              current_password: "",
+              new_password: "",
+              confirm_password: "",
+            }}
+            onSubmit={values => onPressUpdate(values)}>
+            {({handleChange, handleSubmit, values, errors, isValid, dirty}) => (
+              <>
+                <CustomInput
+                  testID="input-current_password"
+                  label="Current Password"
+                  name="current_password"
+                  onChangeText={handleChange('current_password')}
+                  value={values.current_password}
+                  error={errors.current_password}
+                  iconPosition="right"
+                  placeholder="Masukkan password saat ini"
+                />
+                <CustomInput
+                  testID="input-new_password"
+                  label="New Password"
+                  name="new_password"
+                  onChangeText={handleChange('new_password')}
+                  value={values.new_password}
+                  error={errors.new_password}
+                  iconPosition="right"
+                  placeholder="Masukkan password baru"
+                />
+                <CustomInput
+                  testID="input-confirm_password"
+                  label="Confirm Password"
+                  name="confirm_password"
+                  onChangeText={handleChange('confirm_password')}
+                  value={values.confirm_password}
+                  error={errors.confirm_password}
+                  iconPosition="right"
+                  placeholder="Masukkan lagi password baru"
+                />
+                <CustomButton
+                  testID="btn-login"
+                  primary
+                  title="Simpan"
+                  disable={!(dirty && isValid)}
+                  onPress={handleSubmit}
+                />
+              </>
+            )}
+          </Formik>
+        </View>
     </View>
   )
 }
---
+++ src/screens/Pengaturan/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {SIZES} from '../../themes';
+
+export default StyleSheet.create({
+  form: {
+    marginHorizontal: SIZES.padding,
+    marginTop: 24,
+    marginBottom: 30,
+  },
+});
