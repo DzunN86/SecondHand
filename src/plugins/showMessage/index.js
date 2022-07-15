333b71d5de2c8da7d960f0d5e59dd3ff43ab4a3e
Dzun <nurroinz@gmail.com>
2022-07-15 22:26:54 +0700

    add style

--- src/plugins/showMessage/index.js
+++ src/plugins/showMessage/index.js
@@ -1,5 +1,5 @@
 import {showMessage} from 'react-native-flash-message';
-import {COLORS} from '../../themes';
+import {COLORS, FONTS, RADIUS, SIZES} from '../../themes';
 
 export function showError(message) {
   showMessage({
@@ -7,6 +7,18 @@ export function showError(message) {
     type: 'default',
     backgroundColor: COLORS.danger,
     color: COLORS.white,
+    icon: 'danger',
+    duration: 5000,
+    style: {
+      marginTop: SIZES.padding * 2 + 6,
+      marginHorizontal: SIZES.padding,
+      borderRadius: RADIUS.medium,
+      padding: SIZES.base,
+    },
+    titleStyle: {
+      ...FONTS.h5,
+      marginTop: 3,
+    },
   });
 }
 
@@ -16,5 +28,17 @@ export function showSuccess(message) {
     type: 'default',
     backgroundColor: COLORS.success,
     color: COLORS.white,
+    icon: 'success',
+    duration: 5000,
+    style: {
+      marginTop: SIZES.padding * 2 + 6,
+      marginHorizontal: SIZES.padding,
+      borderRadius: RADIUS.medium,
+      padding: SIZES.base,
+    },
+    titleStyle: {
+      ...FONTS.h5,
+      marginTop: 3,
+    },
   });
 }
