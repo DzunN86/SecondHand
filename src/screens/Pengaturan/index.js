c8d9ea20b5aa1d7950bf8d774f0f946806364e5f
Galang Sumantri <galanksumantri@gmail.com>
2022-07-16 07:21:42 +0700

    api

--- src/screens/Pengaturan/index.js
+++ src/screens/Pengaturan/index.js
@@ -33,78 +33,78 @@ export default function Settings({navigation}) {
         onPress={() => navigation.goBack()}
       />
       <View style={styles.form}>
-          <Formik
-            initialValues={{
-              current_password: "",
-              new_password: "",
-              confirm_password: "",
-            }}
-            validationSchema={changePasswordSchema}
-            onSubmit={values => onPressUpdate(values)}>
-            {({handleChange, handleSubmit, values, errors, isValid, dirty}) => (
-              <>
-                <CustomInput
-                  testID="input-current_password"
-                  label="Current Password"
-                  name="current_password"
-                  secureTextEntry={isSecureEntry}
-                  onChangeText={handleChange('current_password')}
-                  value={values.current_password}
-                  error={errors.current_password}
-                  iconPosition="right"
-                  placeholder="Masukkan password saat ini"
-                  icon={
-                    <Hidden 
-                      onPress={() => {setIsSecureEntry(prev => !prev)}} 
-                      name={isSecureEntry ? 'eye-off' : 'eye'} 
-                    />
-                  }
-                />
-                <CustomInput
-                  testID="input-new_password"
-                  label="New Password"
-                  name="new_password"
-                  secureTextEntry={isSecureEntry}
-                  onChangeText={handleChange('new_password')}
-                  value={values.new_password}
-                  error={errors.new_password}
-                  iconPosition="right"
-                  placeholder="Masukkan password baru"
-                  icon={
-                    <Hidden 
-                      onPress={() => {setIsSecureEntry1(prev => !prev)}} 
-                      name={isSecureEntry1 ? 'eye-off' : 'eye'} 
-                    />
-                  }
-                />
-                <CustomInput
-                  testID="input-confirm_password"
-                  label="Confirm Password"
-                  name="confirm_password"
-                  secureTextEntry={isSecureEntry}
-                  onChangeText={handleChange('confirm_password')}
-                  value={values.confirm_password}
-                  error={errors.confirm_password}
-                  iconPosition="right"
-                  placeholder="Masukkan lagi password baru"
-                  icon={
-                    <Hidden 
-                      onPress={() => {setIsSecureEntry2(prev => !prev)}} 
-                      name={isSecureEntry2 ? 'eye-off' : 'eye'} 
-                    />
-                  }
-                />
-                <CustomButton
-                  testID="btn-login"
-                  primary
-                  title="Simpan"
-                  disable={!(dirty && isValid)}
-                  onPress={handleSubmit}
-                />
-              </>
-            )}
-          </Formik>
-        </View>
+        <Formik
+          initialValues={{
+            current_password: "",
+            new_password: "",
+            confirm_password: "",
+          }}
+          validationSchema={changePasswordSchema}
+          onSubmit={values => onPressUpdate(values)}>
+          {({handleChange, handleSubmit, values, errors, isValid, dirty}) => (
+            <>
+              <CustomInput
+                testID="input-current_password"
+                label="Current Password"
+                name="current_password"
+                secureTextEntry={isSecureEntry}
+                onChangeText={handleChange('current_password')}
+                value={values.current_password}
+                error={errors.current_password}
+                iconPosition="right"
+                placeholder="Masukkan password saat ini"
+                icon={
+                  <Hidden 
+                    onPress={() => {setIsSecureEntry(prev => !prev)}} 
+                    name={isSecureEntry ? 'eye-off' : 'eye'} 
+                  />
+                }
+              />
+              <CustomInput
+                testID="input-new_password"
+                label="New Password"
+                name="new_password"
+                secureTextEntry={isSecureEntry}
+                onChangeText={handleChange('new_password')}
+                value={values.new_password}
+                error={errors.new_password}
+                iconPosition="right"
+                placeholder="Masukkan password baru"
+                icon={
+                  <Hidden 
+                    onPress={() => {setIsSecureEntry1(prev => !prev)}} 
+                    name={isSecureEntry1 ? 'eye-off' : 'eye'} 
+                  />
+                }
+              />
+              <CustomInput
+                testID="input-confirm_password"
+                label="Confirm Password"
+                name="confirm_password"
+                secureTextEntry={isSecureEntry}
+                onChangeText={handleChange('confirm_password')}
+                value={values.confirm_password}
+                error={errors.confirm_password}
+                iconPosition="right"
+                placeholder="Masukkan lagi password baru"
+                icon={
+                  <Hidden 
+                    onPress={() => {setIsSecureEntry2(prev => !prev)}} 
+                    name={isSecureEntry2 ? 'eye-off' : 'eye'} 
+                  />
+                }
+              />
+              <CustomButton
+                testID="btn-login"
+                primary
+                title="Simpan"
+                disable={!(dirty && isValid)}
+                onPress={handleSubmit}
+              />
+            </>
+          )}
+        </Formik>
+      </View>
     </View>
   )
 }
--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -14,3 +14,4 @@ export const updateProfile = async (data) => await axios.put('/auth/user', data,
     'Content-Type': 'multipart/form-data',
   },
 });
+export const changePassword = async () => await axios.get('/auth/change-password');
