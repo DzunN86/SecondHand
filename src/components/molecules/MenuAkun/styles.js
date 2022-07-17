c49e5b51090261eb80e3c2361fa31389dfeb92e5
Dzun <nurroinz@gmail.com>
2022-07-17 21:13:15 +0700

    Update styles.js
    Co-Authored-By: qodri12 <87253283+qodri12@users.noreply.github.com>
    Co-Authored-By: Juniza Permata Nugraheni <72010002+junizapermatanugraheni@users.noreply.github.com>

--- src/components/molecules/MenuAkun/styles.js
+++ src/components/molecules/MenuAkun/styles.js
@@ -1,19 +1,20 @@
 import {StyleSheet} from 'react-native';
-import {FONTS} from '../../../themes';
+import {COLORS, FONTS} from '../../../themes';
 
 export default StyleSheet.create({
   menuItem: {
     flexDirection: 'row',
     paddingVertical: 15,
   },
-  separator : {
-    height: 0.5, 
-    width: '100%', 
-    backgroundColor: '#c8c8c8'
+  separator: {
+    height: 0.5,
+    width: '100%',
+    backgroundColor: '#c8c8c8',
   },
   menuItemText: {
     ...FONTS.h4,
     marginLeft: 20,
+    color: COLORS.black,
     alignSelf: 'center',
   },
 });
