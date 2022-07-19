1331e2bfe7630d2bd5e96c88415d3b03b1860a73
Dzun <nurroinz@gmail.com>
2022-07-20 02:55:50 +0700

    add image for empty state

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,5 +1,8 @@
 import product from './product.png';
 import hadiah from './Hadiah.png';
 import MainLogo from './main-logo.png';
+import EmptyOrder from './EmptyOrder.png';
+import EmptyNotif from './EmptyNotif.png';
+import EmptySearch from './EmptySearch.png';
 
-export {product, hadiah, MainLogo};
+export {product, hadiah, MainLogo, EmptyOrder, EmptyNotif, EmptySearch};
