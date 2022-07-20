068ace7e67c715310f5a94943094d92f064bdd06
Dzun <nurroinz@gmail.com>
2022-07-20 09:31:30 +0700

    Update index.js

--- src/screens/Wishlist/index.js
+++ src/screens/Wishlist/index.js
@@ -1,8 +1,12 @@
 import React, {useEffect} from 'react';
 import {ScrollView, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import {CardWishlist, CustomHeader} from '../../components';
-import {deleteItemWishlist, getItemWishlist} from '../../store/actions/wishlist';
+import {EmptyOrder} from '../../assets';
+import {CardWishlist, CustomHeader, EmptyState} from '../../components';
+import {
+  deleteItemWishlist,
+  getItemWishlist,
+} from '../../store/actions/wishlist';
 import styles from './styles';
 
 const Wishlist = ({navigation}) => {
@@ -15,30 +19,37 @@ const Wishlist = ({navigation}) => {
 
   const onDelete = id => {
     dispatch(deleteItemWishlist(id));
-  }
+  };
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <CustomHeader
-          type="BackTitle"
-          title="Wishlist Barang"
-          onPress={() => navigation.goBack()}
-        />
-        <View style={styles.productWrapper}>
-          {dataWishlist.map((item, index) => (
-            <CardWishlist
-              key={index}
-              onPress={() => navigation.navigate('DetailProductScreen', {id_product: item?.product_id})}
-              image={{uri: item?.Product?.image_url}}
-              product_name={item?.Product?.name}
-              base_price={item?.Product?.base_price}
-              location={item?.Product?.location}
-              onDelete={() => onDelete(item?.id)}
-            />
-          ))}
-        </View>
-      </ScrollView>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <CustomHeader
+        type="BackTitle"
+        title="Wishlist Barang"
+        onPress={() => navigation.goBack()}
+      />
+      <EmptyState
+        image={EmptyOrder}
+        title="Tidak ada produk yang diminati"
+        subTitle="Sabar ya rejeki nggak kemana kok"
+      />
+      <View style={styles.productWrapper}>
+        {dataWishlist.map((item, index) => (
+          <CardWishlist
+            key={index}
+            onPress={() =>
+              navigation.navigate('DetailProductScreen', {
+                id_product: item?.product_id,
+              })
+            }
+            image={{uri: item?.Product?.image_url}}
+            product_name={item?.Product?.name}
+            base_price={item?.Product?.base_price}
+            location={item?.Product?.location}
+            onDelete={() => onDelete(item?.id)}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
