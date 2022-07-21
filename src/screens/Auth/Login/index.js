8cc5d55c9f8eeb88140906eed5d144eb651b18e5
qodri12 <qodgaming3@gmail.com>
2022-07-21 15:40:30 +0700

    update biometric auth #3

--- src/screens/Auth/Login/index.js
+++ src/screens/Auth/Login/index.js
@@ -1,10 +1,10 @@
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import React, { useState } from 'react';
 import { CustomHeader, CustomInput, CustomButton } from '../../../components';
 import styles from './styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { doLogin } from '../../../store/actions/auth/loginUser';
-import { loginSchema } from '../../../plugins';
+import { getDataSecure, loginSchema, showError, storeDataSecure } from '../../../plugins';
 import { Formik } from 'formik';
 import { COLORS } from '../../../themes';
 import Icon from 'react-native-vector-icons/Feather';
@@ -15,10 +15,10 @@ export default function Login({ navigation }) {
   const [isSecureEntry, setIsSecureEntry] = useState(true);
   const dispatch = useDispatch();
   const { isLoading } = useSelector(state => state.commonReducers);
-  const { isLogin, userData } = useSelector(state => state.loginReducer);
+  const { userData } = useSelector(state => state.loginReducer);
 
   const onPressLogin = ({ email, password }) => {
-
+    storeDataSecure("user", { email, password })
     dispatch(doLogin(email, password, navigation))
   };
 
@@ -35,15 +35,32 @@ export default function Login({ navigation }) {
       passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
     };
 
-    TouchID.authenticate('to demo this react-native component', optionalConfigObject)
-      .then(success => {
-        console.log(success)
-        alert('Authenticated Successfully, Signed in with Fingerprint!');
-      })
-      .catch(error => {
-        console.log(error)
-        alert('Authentication Failed');
-      });
+    TouchID.isSupported(optionalConfigObject).then((biometryType) => {
+      if (biometryType === 'FaceID') {
+        Alert.alert('perangkat ini support FaceID')
+      } else {
+        TouchID.authenticate("Untuk Melakukan Login", optionalConfigObject)
+          .then(() => {
+            getDataSecure("user")
+              .then((user) => {
+                if (user) {
+                  const akunUser = {
+                    email: user.email,
+                    password: user.password,
+                  }
+                  dispatch(doLogin(akunUser.email, akunUser.password, navigation))
+                } else {
+                  showError("Silahkan Login Terlebih Dahulu")
+                }
+              })
+          })
+          .catch(error => {
+            console.log(error)
+            alert('Authentication Failed');
+          });
+      }
+    })
+
   };
   return (
     <ScrollView contentContainerStyle={styles.scroll} testID="LoginScreen">
@@ -101,7 +118,6 @@ export default function Login({ navigation }) {
                   }
                 />
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-
                   <CustomButton
                     style={{ width: userData.access_token ? "83%" : "100%" }}
                     testID="btn-login"
