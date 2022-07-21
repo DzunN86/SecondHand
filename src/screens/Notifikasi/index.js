8e999c370b9f5afc78d6e5435bbf5489e8976092
Dzun <nurroinz@gmail.com>
2022-07-21 18:33:52 +0700

    pisahkan component

--- src/screens/Notifikasi/index.js
+++ src/screens/Notifikasi/index.js
@@ -1,147 +1,14 @@
-import {
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  FlatList,
-  RefreshControl,
-  Alert,
-} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import styles from './style';
-import {useDispatch, useSelector} from 'react-redux';
-import {getNotification, ReadNotification} from '../../store/actions';
-import {CustomHeader} from '../../components/atoms';
-import {formatDateTime, formatRupiah, sortDate} from '../../utils';
 import {useIsFocused} from '@react-navigation/native';
-import {Skeleton} from '@rneui/base';
-import {COLORS, RADIUS} from '../../themes';
-import {EmptyState} from '../../components';
+import React, {useEffect, useState} from 'react';
+import {Alert, FlatList, RefreshControl, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import {EmptyNotif} from '../../assets';
+import {CardNotif, EmptyState, LoadingNotif} from '../../components';
+import {CustomHeader} from '../../components/atoms';
 import {showSuccess} from '../../plugins';
-
-function CardNotif({
-  image_url,
-  status,
-  product_name,
-  base_price,
-  bid_price,
-  date,
-  isRead,
-  onPress,
-}) {
-  const titleNotif = () => {
-    switch (status) {
-      case 'accepted':
-        return 'Penawaranmu Telah Diterima';
-      case 'create':
-        return 'Berhasil di terbitkan';
-      case 'bid':
-        return 'Penawaran Produk';
-      case 'declined':
-        return 'Penawaran Ditolak';
-      case 'Dibatalkan':
-        return 'Penawaran Ditolak';
-      default:
-        return '';
-    }
-  };
-  return (
-    <>
-      <TouchableOpacity onPress={onPress}>
-        <View style={styles.productNotification}>
-          <Image source={{uri: image_url}} style={styles.productImage}></Image>
-          <View style={styles.productInfo}>
-            <View style={styles.wrapperDate}>
-              <Text style={styles.label}>{titleNotif()}</Text>
-              <View style={styles.wrapper}>
-                <Text style={styles.label}>{formatDateTime(date)}</Text>
-                {!isRead && <View style={styles.isRead} />}
-              </View>
-            </View>
-            <Text style={styles.labelText}>{product_name}</Text>
-            <Text
-              style={[
-                styles.labelText,
-                bid_price ? styles.strike_through : null,
-              ]}>
-              {formatRupiah(base_price)}
-            </Text>
-            {bid_price && (
-              <Text style={styles.labelText}>
-                {'Ditawar ' + formatRupiah(bid_price)}
-              </Text>
-            )}
-            {status == 'accepted' && (
-              <Text style={styles.label}>
-                Kamu akan segera dihubungi penjual via whatsapp
-              </Text>
-            )}
-          </View>
-        </View>
-      </TouchableOpacity>
-    </>
-  );
-}
-
-function LoadingNotif() {
-  return (
-    <View style={styles.productNotification}>
-      <Skeleton
-        animation="pulse"
-        width={60}
-        height={60}
-        backgroundColor={COLORS.grey7}
-        skeletonStyle={{backgroundColor: COLORS.grey3}}
-        style={{
-          borderRadius: RADIUS.small,
-        }}
-      />
-      <View style={styles.productInfo}>
-        <View style={styles.wrapperDate}>
-          <Skeleton
-            animation="pulse"
-            width={150}
-            backgroundColor={COLORS.grey7}
-            skeletonStyle={{backgroundColor: COLORS.grey3}}
-            style={{
-              borderRadius: RADIUS.small,
-            }}
-          />
-          <Skeleton
-            animation="pulse"
-            width={50}
-            backgroundColor={COLORS.grey7}
-            skeletonStyle={{backgroundColor: COLORS.grey3}}
-            style={{
-              borderRadius: RADIUS.small,
-            }}
-          />
-        </View>
-        <Skeleton
-          animation="pulse"
-          width={100}
-          backgroundColor={COLORS.grey7}
-          skeletonStyle={{backgroundColor: COLORS.grey3}}
-          style={{
-            marginTop: 15,
-            borderRadius: RADIUS.small,
-          }}
-        />
-        <Skeleton
-          animation="pulse"
-          width={70}
-          backgroundColor={COLORS.grey7}
-          skeletonStyle={{backgroundColor: COLORS.grey3}}
-          style={{
-            marginTop: 15,
-            borderRadius: RADIUS.small,
-          }}
-        />
-      </View>
-    </View>
-  );
-}
+import {getNotification, ReadNotification} from '../../store/actions';
+import {sortDate} from '../../utils';
+import styles from './style';
 
 export default function Notifikasi({navigation}) {
   const {notif, isLoading} = useSelector(state => state.notificationReducer);
@@ -176,6 +43,26 @@ export default function Notifikasi({navigation}) {
         }
         break;
       case 'declined':
+        Alert.alert('Tawar Lagi?', 'Apakah anda menawar lagi?', [
+          {text: 'Tidak', style: 'cancel'},
+          {
+            text: 'Ya',
+            onPress: () => {
+              if (item?.Product !== null) {
+                navigation.navigate('DetailProductScreen', {
+                  id: item?.Product?.id,
+                });
+              } else {
+                Alert.alert(
+                  'Barang tidak ditemukan',
+                  'Barang yang ingin anda tawarkan tidak ditemukan, mungkin telah dihapus',
+                );
+              }
+            },
+          },
+        ]);
+        break;
+      case 'tolak':
         Alert.alert('Tawar Lagi?', 'Apakah anda menawar lagi?', [
           {text: 'Tidak', style: 'cancel'},
           {
@@ -237,8 +124,6 @@ export default function Notifikasi({navigation}) {
       <CustomHeader type="HeaderTitle" title="Notifikasi" />
       {isLoading ? (
         <>
-          <LoadingNotif />
-          <LoadingNotif />
           <LoadingNotif />
           <LoadingNotif />
         </>
